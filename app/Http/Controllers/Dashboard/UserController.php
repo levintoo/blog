cfc8c2590146f5d8c4290d82ae20e05619fb74a9
levintoo <tooklevin@gmail.com>
2024-03-25 18:56:03 +0300

    create users in dashboard

--- app/Http/Controllers/Dashboard/UserController.php
+++ app/Http/Controllers/Dashboard/UserController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUserRequest;
 use App\Models\User;
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -14,7 +17,15 @@ class UserController extends Controller
      */
     public function index()
     {
-        $users = User::withTrashed()->Paginate();
+        $users = User::withTrashed()->Paginate()->through(fn($user) => [
+            'name' => $user->name,
+            'email' => $user->email,
+            'id' => $user->id,
+            'verified' => !!$user->email_verified_at,
+            'created' => $user->created_at ? $user->created_at->format('d/m/Y H:i T') : null,
+            'updated' => $user->updated_at ? $user->updated_at->format('d/m/Y H:i T') : null,
+            'trashed' => $user->deleted_at ? $user->deleted_at->format('d/m/Y H:i T') : null,
+        ]);
         return inertia('Dashboard/Users/Users', compact('users'));
     }
 
@@ -23,15 +34,22 @@ class UserController extends Controller
      */
     public function create()
     {
-        //
+        return inertia('Dashboard/Users/Create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreUserRequest $request)
     {
-        //
+        $validated = $request->validated();
+
+        User::create([
+            ...$validated,
+            'password' => Hash::make($validated['password']),
+        ]);
+
+        return redirect()->route('users.index');
     }
 
     /**
@@ -61,15 +79,26 @@ class UserController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(String $id)
+    public function softDestroy(String $id)
     {
         $user = User::findOrFail($id);
 
-        if($user->id === Auth::id() || $user->id == config('app.allowed_email')) {
+        if($user->id === Auth::id() || $user->email == config('app.admin_email')) {
             abort(403);
         }
 
         $user->delete();
+    }
+
+    public function destroy(String $id)
+    {
+        $user = User::findOrFail($id);
+
+        if($user->id === Auth::id() || $user->email == config('app.admin_email')) {
+            abort(403);
+        }
+
+        $user->forceDelete();
     }
 
     public function restore(String $id)
